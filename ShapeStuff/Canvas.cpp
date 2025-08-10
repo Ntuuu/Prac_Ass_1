507eb055a26dcdf560ad50237ff82010ccce9cfb
morganthegirlboss <u24910882@tuks.co.za>
2025-08-10 22:15:03 +0200

    basic canvas implementation

--- ShapeStuff/Canvas.cpp
+++ ShapeStuff/Canvas.cpp
@@ -1,1 +1,18 @@
 #include "Canvas.h"
+
+Canvas::Canvas(){
+
+    this->shapes = NULL;
+
+}
+
+Canvas::~Canvas(){
+    if(this->shapes != NULL){
+        delete[] this->shapes;
+        this->shapes = NULL;
+    }
+}
+
+// Canvas::Canvas(const Canvas& other){
+
+// }
